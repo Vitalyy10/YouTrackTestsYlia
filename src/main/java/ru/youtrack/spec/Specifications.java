a8756945fb4bca9d7e826af8cdb109f064ceb725
Vitalyy10 <Shevchukovv12@mail.ru>
2026-03-14 01:36:15 +0300

    Тест интернал порта

--- src/main/java/ru/youtrack/spec/Specifications.java
+++ src/main/java/ru/youtrack/spec/Specifications.java
@@ -27,7 +27,7 @@ public class Specifications {
         Set<String> blacklistHeader = new HashSet<>();
         blacklistHeader.add("Authorization");
 
-        requestBuilder.setBaseUri("http://localhost:8081/api");
+        requestBuilder.setBaseUri("http://host.docker.internal:8081/api");
         // Временно включаю лоигрование токена
         requestBuilder.addFilter(new RequestLoggingFilter(LogDetail.ALL, true, System.out, false, blacklistHeader));
         requestBuilder.addFilter(new ResponseLoggingFilter());
